c8d3977d860edfd6554fb489c78a8300e6160ffd
Jing Wang <yuzhounh@gmail.com>
2025-09-13 11:56:08 +0000

    将以上页面的宽度增加20%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CategoryPage from '@/components/search/CategoryPage';
 export default function Home() {
   return (
     <main className="min-h-screen bg-background text-foreground">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <CategoryPage />
       </div>
     </main>
