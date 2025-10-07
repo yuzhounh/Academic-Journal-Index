db96ac1936049a9639f6fe40a816373c4c1a47fc
Jing Wang <yuzhounh@gmail.com>
2025-10-07 12:53:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "journal-information-5664-d4572",
+  "appId": "1:851079863796:web:83e767c3c55ee1d750ada1",
+  "apiKey": "AIzaSyBe1K9utkMdXqPvIsJVZJXn8E_nv6seiu8",
+  "authDomain": "journal-information-5664-d4572.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "851079863796"
+};
