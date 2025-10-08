82a78810f27db32917631efccdd739e1c863e639
Jing Wang <yuzhounh@gmail.com>
2025-10-08 13:30:51 +0000

    Github 给我发邮件，提示如下：

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "journal-information-5664-d4572",
   "appId": "1:851079863796:web:83e767c3c55ee1d750ada1",
-  "apiKey": "AIzaSyBe1K9utkMdXqPvIsJVZJXn8E_nv6seiu8",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "journal-information-5664-d4572.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "851079863796"
